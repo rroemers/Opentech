6aa615b80883ca32f4b3156871e13c138eb6db96
rroemers <rob@roemers.be>
2014-03-24 23:53:32 +0100

    first go at honeycombing

--- Hexagons.py
+++ Hexagons.py
@@ -4,13 +4,15 @@ side_l = 50 #side lenght
 side_s = 6 #side number
 side_a = 60 #side angle
 
-shape_n = 1 #number of shapes
+shape_n = 3 #number of shapes
 
 def draw_side():
 	turtle.forward(side_l)
 	turtle.left(side_a)
 
-for h in range(side_s):
-	draw_side()
+for n in range(shape_n):
+	for h in range(side_s):
+		draw_side()
+	turtle.forward(side_l)
 
 turtle.exitonclick()
